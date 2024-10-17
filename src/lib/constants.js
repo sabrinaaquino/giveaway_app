a9e634ce8f1027d2eed642bcfcc619625f3476b3
sabrinaaquino <sabrina.aquino@hotmail.com>
2024-10-17 10:41:08 -0300

    new database

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -2,6 +2,6 @@ export const APPWRITE_ENDPOINT = 'https://cloud.appwrite.io/v1';
 
 export const APPWRITE_PROJECT = '670ef5e50038f6e27037';
 
-export const DATABASE_NAME = '670ef9c20033ec6a08d8';
+export const DATABASE_NAME = '6711135200352164e38a';
 
-export const COLLECTION_NAME = '671111d000289afcd037'
+export const COLLECTION_NAME = '6711135f000a1cd92559';
