f1e475efc669d4f09fe63595add9c784c3980310
sabrinaaquino <sabrina.aquino@hotmail.com>
2024-10-17 10:36:03 -0300

    new collection

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -4,4 +4,4 @@ export const APPWRITE_PROJECT = '670ef5e50038f6e27037';
 
 export const DATABASE_NAME = '670ef9c20033ec6a08d8';
 
-export const COLLECTION_NAME = '670ef9e3002744214743'
+export const COLLECTION_NAME = '671111d000289afcd037'
