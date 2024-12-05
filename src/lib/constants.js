e9a130a3b43f66e43a596d5ea1a9f915098789e5
sabrinaaquino <sabrina.aquino@hotmail.com>
2024-12-05 10:52:53 -0300

    updating the collection

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -4,4 +4,4 @@ export const APPWRITE_PROJECT = '671115fa0006155ac867';
 
 export const DATABASE_NAME = '6711165500170baafafb';
 
-export const COLLECTION_NAME = '671116630031cc1becc2';
+export const COLLECTION_NAME = '67510ada000d93e48530';
